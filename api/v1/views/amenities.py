58b40d4ff1a65d0e8989bd9db496a7caa2476419
rmf10003 <685@holbertonschool.com>
2019-09-11 19:34:08 +0000

    pep8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -15,6 +15,7 @@ def amenity_all():
         amenity_list.append(value.to_dict())
     return (jsonify(amenity_list))
 
+
 @app_views.route('/amenities/<amenity_id>', methods=['GET'])
 def amenity_grab(amenity_id):
     """grab one amenity based on ID"""
@@ -24,6 +25,7 @@ def amenity_grab(amenity_id):
         if id == amenity_id:
             return (jsonify(amenities.get(key).to_dict()))
     abort(404)
+
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
 def amenity_del(amenity_id):
@@ -37,31 +39,33 @@ def amenity_del(amenity_id):
             return (jsonify({}))
     abort(404)
 
+
 @app_views.route('/amenities', strict_slashes=False, methods=['POST'])
 def amenity_create():
     """Creates a amenity Object"""
     if not request.get_json():
-         abort(400, "Not a JSON")
-    if not 'name' in request.get_json():
-         abort(400, "Missing name")
+        abort(400, "Not a JSON")
+    if 'name' not in request.get_json():
+        abort(400, "Missing name")
     name = request.get_json()
     amenity = models.amenity.Amenity(**name)
     models.storage.new(amenity)
     models.storage.save()
     return (jsonify(amenity.to_dict()))
 
+
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'])
 def amenity_change(amenity_id):
     """update a amenity object"""
     if not request.get_json():
-       abort(400, "Not a JSON")
+        abort(400, "Not a JSON")
     amenities = models.storage.all('Amenity')
     for key in amenities.keys():
         s, p, id = key.partition('.')
         if id == amenity_id:
             for k, v in request.get_json().items():
                 if k not in ('id', 'created_at', 'updated_at'):
-                    setattr(amenities[key], k, v) 
+                    setattr(amenities[key], k, v)
             models.storage.save()
             return (jsonify(amenities[key].to_dict()))
-    abort(404)    
+    abort(404)
---
+++ api/v1/views/users.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Module to handle state restful API actions"""
+
+import models
+from flask import Flask, jsonify, request, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/users', strict_slashes=False, methods=['GET'])
+def user_all():
+    """Retrives a list of all users objects"""
+    users = models.storage.all('User')
+    user_list = []
+    for value in users.values():
+        user_list.append(value.to_dict())
+    return (jsonify(user_list))
+
+
+@app_views.route('/users/<user_id>', methods=['GET'])
+def user_grab(user_id):
+    """grab one user based on ID"""
+    users = models.storage.all('User')
+    for key in users.keys():
+        s, p, id = key.partition('.')
+        if id == user_id:
+            return (jsonify(users.get(key).to_dict()))
+    abort(404)
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def user_del(user_id):
+    """delete a user object"""
+    users = models.storage.all('User')
+    for key in users.keys():
+        s, p, id = key.partition('.')
+        if id == user_id:
+            models.storage.delete(users.get(key))
+            models.storage.save()
+            return (jsonify({}))
+    abort(404)
+
+
+@app_views.route('/users', strict_slashes=False, methods=['POST'])
+def user_create():
+    """Creates a user Object"""
+    if not request.get_json():
+        abort(400, "Not a JSON")
+    if 'email' not in request.get_json():
+        abort(400, "Missing email")
+    if 'password' not in request.get_json():
+        abort(400, "Missing password")
+    name = request.get_json()
+    user = models.user.User(**name)
+    models.storage.new(user)
+    models.storage.save()
+    return (jsonify(user.to_dict()))
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def user_change(user_id):
+    """update a user object"""
+    if not request.get_json():
+        abort(400, "Not a JSON")
+    users = models.storage.all('User')
+    for key in users.keys():
+        s, p, id = key.partition('.')
+        if id == user_id:
+            for k, v in request.get_json().items():
+                if k not in ('email', 'id', 'created_at', 'updated_at'):
+                    setattr(users[key], k, v)
+            models.storage.save()
+            return (jsonify(users[key].to_dict()))
+    abort(404)
